04ea98cdf88b1f7d67e9f061868e6fe24cfa8784
ma.tanaka <tanaka@marietta.co.jp>
2012-02-01 17:38:52 +0900

    ANE一式追加

---
+++ ANE/batch.sh
@@ -0,0 +1,1 @@
+adt -package -target ane ANEHelloWorldExtension.ane extension.xml -swc ANEHelloWorldExtension.swc -platform iPhone-ARM -C iPhone-ARM . -platform Android-ARM -C Android-ARM . -platform default -C default .
