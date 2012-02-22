41ab0cab1b687174622b2287cc9c17ad3a67163f
Tanaka Yuuya <tnker0217@gmail.com>
2012-02-22 11:38:55 +0900

    変更テスト

--- ane/batch.sh
+++ ane/batch.sh
@@ -1,1 +1,3 @@
 adt -package -target ane ANEHelloWorldExtension.ane extension.xml -swc ANEHelloWorldExtension.swc -platform iPhone-ARM -C iPhone-ARM . -platform Android-ARM -C Android-ARM . -platform default -C default .
+
+aa
